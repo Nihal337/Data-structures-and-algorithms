d921d7ee2f595ea2bf9bea7dd7cfb3afc316277b
Nihal Tiwari <83083841+Nihal337@users.noreply.github.com>
2022-02-14 21:15:25 +0530

    Update minDepth.cpp

--- Binary-Trees/minDepth.cpp
+++ Binary-Trees/minDepth.cpp
@@ -4,23 +4,23 @@
 
 
 
-           int minDepth(Node *root) {
-     	
-             //base case
+        int minDepth(TreeNode* root) {
+            //base case
+        if(root==NULL) return 0 ;
         if(root->left==NULL && root->right==NULL) return 1 ;
         
         
              // rec case
-        int d1=INT_MAX,d2=INT_MAX ;
+        int d1=100005,d2=100005 ;
         
         if(root->left)
          d1 = minDepth(root->left) ;
         
         if(root->right)
-        int d2 = minDepth(root->right) ;
+         d2 = minDepth(root->right) ;
         
              // self work
         return 1+min(d1,d2) ;
         
-}
+    }
      
