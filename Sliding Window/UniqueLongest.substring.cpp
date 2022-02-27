795584cfab944ba5f710681d61b5538060a80cc4
Nihal Tiwari <83083841+Nihal337@users.noreply.github.com>
2022-02-27 21:19:05 +0530

    Update UniqueLongest.substring.cpp

--- Sliding Window/UniqueLongest.substring.cpp
+++ Sliding Window/UniqueLongest.substring.cpp
@@ -37,3 +37,50 @@ string Unique_Longest_Substring() {
      }
      
      cout<<maxans<<endl ;
+  
+  
+  
+  
+  --------------------------------------------------------------------------------
+    // mY apporoach 2 : using sliding window
+    
+    void LongestUnique_Substring(string s) {
+		int n= s.size() ;
+		
+		  int i=0 ,j=0 ;
+		  int ans_i =0 ,ans_j =0 ;
+		  map<char,int>mp ;
+		    int diff = 0 ;
+		  
+		  while(j<n-1) {
+		  	
+		  		//  expand the Window 
+		  	mp[s[j]]++ ;
+		  	j++ ;
+		  	
+		  	
+		  	
+		  	// what if curr char is already present in my window
+		  	// create new Window 
+		  	if(mp[s[j]] == 1) {
+		  		mp.clear() ;
+		  		i=j;
+		  	}
+		  	
+		  	// update the ans at the given time
+		  	   if((j-i+1)>diff) {
+		  	   	ans_i = i ;
+		  	   	ans_j = j ;
+		  	   	diff = j-i+1 ;
+		  	   
+		  	   }	
+		  	
+		  }
+		 
+		
+		  while(ans_i<=ans_j) {
+		  	cout<<s[ans_i] ;
+		  	ans_i++ ;
+		  }
+	}
+	-----------------------------------------------------------------------------------
